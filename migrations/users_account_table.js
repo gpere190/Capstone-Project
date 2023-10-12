d71f6d5b9aea929d99b126102d5839fabfd03211
Gricelle Perez <gperez@xtec.com>
2023-10-12 10:17:33 -0400

    adding the migration tables for the user accounts

---
+++ migrations/users_account_table.js
@@ -0,0 +1,29 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+// Migration for User Accounts
+exports.up = function (knex) {
+  return knex.schema.createTable("users", (table) => {
+    table.increments("id").primary();
+    table.string("username").notNullable().unique();
+    table.string("email").notNullable().unique();
+    table.string("password").notNullable();
+  });
+};
+exports.down = function (knex) {
+  return knex.schema.dropTable("users");
+};
+
+// // Migration for Authentication Tokens
+// exports.up = function (knex) {
+//   return knex.schema.createTable("auth_tokens", function (table) {
+//     table.increments("id").primary();
+//     table.integer("user_id").unsigned().notNullable();
+//     table.string("token").notNullable().unique();
+//     table.timestamp("expires_at").notNullable();
+//   });
+// };
+// exports.down = function (knex) {
+//   return knex.schema.dropTable("auth_tokens");
+// };
